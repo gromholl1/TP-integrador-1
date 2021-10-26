60a79c49efa5ade8b97d7486344b491eb25f43af
Your Name <you@example.com>
2021-10-26 18:06:50 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var cad=`
+<div class="logo">
+<img src="./imag/codoacodo.png" alt="logo">
+<h3 class="confbsas">Conf Bs As</h3>
+</div>
+<div class="nav">
+<a href="#foto" class="links1">La conferencia</a>
+<a href="#conoce" class="links2">Los oradores</a>
+<a href="#hawaioctubre" class="links2">El lugar y la fecha</a>
+<a href="#comentarios" class="links2">Conviértete en orador</a>
+<a href="./compraticket.html" target="_blanck" class="links3">Comprar tickets</a>
+</div>`
+
+document.getElementById("header").innerHTML=cad
+
+cad=`
+<nav class="navpie">
+                <a href="preguntas.html" target="_blanck" class="pie">Preguntas <br> frecuentes</a>
+                <a href="contacto.html" target="_blanck" class="pie">Contáctanos</a>
+                <a href="prensa.html" target="_blanck" class="pie">Prensa</a>
+                <a href="conferencias.html" target="_blanck" class="pie">Conferencias</a>
+                <a href="terminos.html" target="_blanck" class="pie">Términos y <br> condiciones</a>
+                <a href="privacidad.html" target="_blanck" class="pie">Privacidad</a>
+                <a href="estudiantes.html" target="_blanck" class="pie">Estudiantes</a>
+            </nav>
+`
+document.getElementById("footer").innerHTML=cad
